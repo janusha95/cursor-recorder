2d2352c01b077a4a8d151eb9def4c37b77fb2579
janusha95 <60120739+janusha95@users.noreply.github.com>
2020-06-12 01:10:13 -0400

    added html and styling for box

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+var cur = document.getElementById("cur");
+
+start.addEventListener("click", record);  
+stop.addEventListener("click", stop);
+
+function record() {
+  // Add functionality for record or replay
+}
+  
+function stop() {
+    // Add functionality for stop
+}
+
+function record() {
+  // Add functionality for record
+}
+
+function play() {
+  // Add functionality for play
+}
+
+
