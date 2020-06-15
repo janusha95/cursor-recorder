6e388a24eb1cbd955c16c19a24f6885883e18088
janusha95 <60120739+janusha95@users.noreply.github.com>
2020-06-14 21:31:47 -0400

    record n replay functionality

--- js/main.js
+++ js/main.js
@@ -1,24 +1,61 @@
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 var cur = document.getElementById("cur");
+var values = [];
+var i = 0;
+var length = 0;
+var r;
 
-start.addEventListener("click", record);  
-stop.addEventListener("click", stop);
+start.addEventListener("click", recordOrreplay);  
+stop.addEventListener("click", stopOrclear);
 
-function record() {
-  // Add functionality for record or replay
+function recordOrreplay() {
+    if (start.firstChild.textContent === "START RECORDING") {
+      document.addEventListener("mousemove", record);
+      start.removeEventListener("click", recordOrreplay);
+    } else {
+      start.removeEventListener("click", recordOrreplay);
+      i = 0;
+      cur.style.display = "inline-block";
+      cur.style.top = values[i][1];
+      cur.style.left = values[i][0];
+      r = setInterval(play, 10);
+    }
 }
   
-function stop() {
-    // Add functionality for stop
+function stopOrclear() {
+    if (stop.firstChild.textContent === "STOP RECORDING" && values.length > 0) {
+      document.removeEventListener("mousemove", record);
+      length = i;
+      i = 0;
+      document.querySelector("#start p").textContent = "REPLAY";
+      document.querySelector("#stop p").textContent = "CLEAR";
+      start.addEventListener("click", recordOrreplay);
+    } else {
+      i = 0;
+      length = 0;
+      values = [];
+      cur.style.display = "none";
+      document.querySelector("#start p").textContent = "START RECORDING";
+      document.querySelector("#stop p").textContent = "STOP RECORDING";
+    }
 }
 
-function record() {
-  // Add functionality for record
+function record(e) {
+  values[i] = [];
+  values[i][0] = e.clientX;
+  values[i][1] = e.clientY;
+  i++;
 }
 
 function play() {
-  // Add functionality for play
+  if (i == length) {
+    clearInterval(r);
+    start.addEventListener("click", recordOrreplay);
+  } else {
+    cur.style.top = values[i][1] + "px";
+    cur.style.left = values[i][0] + "px";
+    i++;
+  }
 }
-
 
